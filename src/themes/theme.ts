057f5b9d7cfab45f78de5c0d5869829a899c4880
vlopp <vloppworks@gmail.com>
2020-04-18 21:54:19 +0200

    Styled-components theme properly augmented

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,16 +1,20 @@
 import { createMuiTheme, Theme } from "@material-ui/core/styles";
 
-declare module "@material-ui/core/styles/createMuiTheme" {
-  export interface ThemeOptions extends Partial<CustomTheme> {}
-  export interface Theme extends CustomTheme {}
-  export interface DefaultTheme extends CustomTheme {}
-}
-
 interface CustomTheme {
   customZIndexes: {
     settingsOverlay: number;
   };
   scrollbarWidth: string;
+}
+
+declare module "@material-ui/core/styles/createMuiTheme" {
+  export interface ThemeOptions extends Partial<CustomTheme> {}
+  export interface Theme extends CustomTheme {}
+  //export interface DefaultTheme extends CustomTheme {}
+}
+
+declare module "styled-components" {
+  export interface DefaultTheme extends Theme, CustomTheme {}
 }
 
 export const theme = createMuiTheme({
